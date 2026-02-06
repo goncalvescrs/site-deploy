1375a2aa8dabf4d358038635c91fdeb9f24ca07b
cristiano g. <cristianog.contato@gmail.com>
2026-02-06 11:08:20 -0300

    refactor: use `next-connect` in `/status` and `/mirations`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,34 +1,14 @@
 import { createRouter } from "next-connect";
+import controller from "infra/controller";
 import database from "infra/database.js";
-import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
 
 const router = createRouter();
 
-router.get(status);
+router.get(getHandler);
 
-export default router.handler({
-  onNoMatch: onNoMatchHandler,
-  onError: onErrorHandler,
-});
+export default router.handler(controller.errorHandlers);
 
-function onNoMatchHandler(request, response) {
-  const publicErrorObject = new MethodNotAllowedError();
-  console.log(publicErrorObject);
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
-
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-
-  console.log("\n -- Erro dentro do catch do next-connect:");
-  console.log(publicErrorObject);
-
-  response.status(500).json(publicErrorObject);
-}
-
-async function status(request, response) {
+async function getHandler(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
